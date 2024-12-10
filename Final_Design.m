ba246252b2fe9132d9bffa46c254abdaebaf1523
RichardBui1030 <116927755+RichardBui1030@users.noreply.github.com>
2024-12-10 13:15:41 -0600

    changes by richard

--- Final_Design.m
+++ Final_Design.m
@@ -2,11 +2,14 @@ mu = 1.327E11;
 r1 = 1.496E8;
 r2 = 7.786E8;
 n = sqrt(mu/r2^3);
-t12 = pi * sqrt( (r1 + r2)^3 / (8 * mu));
+t12 = pi * sqrt(((r1+200) + r2)^3 / (8 * mu));
+TofDay = t12 / 1.15741*10^-5;
 
-Lead_angle = pi - n*t12;
+Lead_angle = (pi - n*t12) * (180/pi);
 
 dv = sqrt((2*mu/r1)-(2*mu/(r1+r2))) - sqrt(mu/r1);
 
-ToF = 
+
+
+
 
